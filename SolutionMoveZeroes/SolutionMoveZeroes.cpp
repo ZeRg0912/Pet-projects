9d170947bf6e0725c0243262045b5938892f7710
ZeRg0912 <serg.021@yandex.ru>
2023-11-05 16:48:27 +0300

    add gitignore and function zeroes to right in arrays

---
+++ SolutionMoveZeroes/SolutionMoveZeroes.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+void printArr(int* arr) {
+    for (int i = 0; i < 5; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int arr[5]{0, 2, 4, 0, 5};
+
+    int index = 0;
+
+    printArr(arr);
+
+    for (int i = 0; i < 5; i++) {
+        if (arr[i] != 0) {
+            arr[index++] = arr[i];
+        }
+    }
+
+    while (index < 5) {
+        arr[index++] = 0;
+    }
+
+    printArr(arr);
+    
+    return 0;
+}
+
