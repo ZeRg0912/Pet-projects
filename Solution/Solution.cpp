b4463eb277302319a152ea18100a73c6ab5507bb
ZeRg0912 <serg.021@yandex.ru>
2023-11-18 16:41:19 +0300

    solution for non sorted vector

---
+++ Solution/Solution.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+void show(vector<int> in) {
+    for (auto i : in) {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
+void removeDuplicates(std::vector<int>& in)
+{
+    auto end = in.end();
+    for (auto i = in.begin(); i != end; ++i) {
+        end = remove(i + 1, end, *i);
+    }
+    in.erase(end, in.end());
+}
+
+int main()
+{
+    vector<int> v = { 1, 2, 5, 50, 47, 2 ,1, 3, 5, 2, 1, 50, 47, 8, 23, 1, 8 };
+    show(v);
+    removeDuplicates(v);
+    show(v);
+
+    return 0;
+}
