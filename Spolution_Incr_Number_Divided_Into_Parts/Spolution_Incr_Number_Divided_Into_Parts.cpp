7b1ac187347f9fcfbc7129866ca4925b6b9c019c
ZeRg0912 <serg.021@yandex.ru>
2023-11-19 14:54:00 +0300

    add solution for incr array numbers like an int number

---
+++ Spolution_Incr_Number_Divided_Into_Parts/Spolution_Incr_Number_Divided_Into_Parts.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+void Solution(int* array, int size, int incr) {
+	size -= 1;
+	array[size] += incr;
+	if (array[size] >= 10) {
+		int temp_num = array[size] / 10;
+		array[size] = array[size] % 10;
+		Solution(array, size, temp_num);
+	}
+}
+
+void show(int* arr, int size) {
+	for (int i = 0; i < size; ++i) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
+int main()
+{
+	int array[] = {1, 2, 8, 9};
+	int size = sizeof(array) / sizeof(array[0]);
+	show(array, size);
+	Solution(array, size, 1572);
+	show(array, size);
+
+	return 0;
+}
