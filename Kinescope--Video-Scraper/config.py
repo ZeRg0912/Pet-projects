d8f1a01bc2035e96909ed996d8c77201b256f4c7
ZeRg0912 <serg.021@yandex.ru>
2023-11-26 12:46:46 +0300

    add Video-scraper and merge, fix code, packages and add FAQ

---
+++ Kinescope--Video-Scraper/config.py
@@ -0,0 +1,8 @@
+URL = 'https://netology.ru/profile/program/cppm-14/lessons/314171/lesson_items/1701089'
+EMAIL = 'serg.021@yandex.ru'
+PASS_PATH = 'data/password.srt'
+OUTPUT = 'output_mix_video.mp4'
+LINKS_LOG = 'data/logs/links_log.txt'
+FFMPEG_LOG = 'data/logs/ffmpeg_log.txt'
+STEP = 150
+DURATION = 180
---
+++ Kinescope--Video-Scraper/kinescraper/kinescraper.py
@@ -0,0 +1,197 @@
+import datetime
+import subprocess
+import threading
+import time
+import os
+import json
+import curses
+import re
+import ffmpeg
+
+import requests
+from selenium.webdriver.common.keys import Keys
+
+from .file_joiner import join_files
+
+FFMPEG_CMD = 'kinescraper/merge.txt'
+
+
+def scrape(scraper, duration, step):
+    stdscr = curses.initscr()
+    curses.cbreak()
+    stdscr.keypad(True)
+    curses.halfdelay(1 * 10)
+
+    scraper.driver.switch_to.active_element.send_keys(Keys.SPACE)
+    start_time = datetime.datetime.now()
+    result = []
+
+    def wind_forward(scraper, flag):
+        while not flag.is_set():
+            time.sleep(5)
+            for _ in range(step):
+                scraper.driver.switch_to.active_element.send_keys(Keys.ARROW_RIGHT)
+
+    def detect_requests(scraper, flag, result):
+        print(f'Detecting requests from kinescope... ({duration} seconds)\n'
+              f'Press "q" to terminate manually\n')
+        def process_browser_log_entry(entry):
+            response = json.loads(entry['message'])['message']
+            return response
+
+        while not flag.is_set():
+            browser_log = scraper.driver.get_log('performance')
+            events = [process_browser_log_entry(entry) for entry in browser_log]
+            events = [event for event in events if 'Network.response' in event['method']]
+            if events:
+                if events[-1]['params'].get('response'):
+                    link = events[-1]['params']['response']['url']
+                    if 'videos' in link and link not in result:
+                        result.append(link)
+        return result
+
+    timer_flag = threading.Event()
+    detector_flag = threading.Event()
+
+    timer = threading.Thread(target=wind_forward, args=(scraper, timer_flag))
+    timer.start()
+
+    detector = threading.Thread(target=detect_requests, args=(scraper, detector_flag, result))
+    detector.start()
+    
+    print('')
+    stdscr.addstr(f'\n\nPress any key, if finished earlier!\n')
+    while (datetime.datetime.now() - start_time).total_seconds() < duration:
+        remaining_time = round(duration - (datetime.datetime.now() - start_time).total_seconds())
+        if remaining_time > 0:
+            # print(f'\r{remaining_time} seconds remain   ', end='')
+            stdscr.addstr(f'\r{remaining_time} seconds remain   ')
+
+        c = stdscr.getch()
+        if c != curses.ERR:
+            timer_flag.set()
+            detector_flag.set()
+            break
+    else:
+        print("\rTime's up!" + ' ' * 30)
+        timer_flag.set()
+        detector_flag.set()
+
+    scraper.driver.minimize_window()
+    curses.nocbreak()
+    stdscr.keypad(False)
+    curses.echo()
+    curses.endwin()
+    return result
+
+
+def download(urls, links_log='links_log'):
+    print(f'\n{len(urls)} unique requests detected.')
+    
+    with open(links_log, 'wt') as f:
+        for url in urls:
+            f.write(url + '\n')
+
+    files = []
+    audio_counter = 0
+    video_counter = 0
+    resolutions = {}
+
+    for url in urls:
+        if 'audio' not in url:
+            res = url.partition('?')[0].rpartition('/')[-1].rstrip('mp4')
+            resolutions[res] = resolutions.get(res, 0) + 1
+    print(f'Following chunks resolutions detected: {resolutions}')
+    
+    def choose_resolution(resolutions):
+        """
+        Accepts a dictionary {resolution(str): count(int), ...}
+        Checks if there any repeated values in the dictionary (i.e. there equal amounts of chunks of different resolutions)
+        if there are:
+            takes the highest resolution (takes the digits away and compares)
+        else:
+            takes the resolution with the max(count)
+        return: resolution: str
+        """
+        if tuple(set(resolutions.values())) != resolutions.values():
+            choose_from = {res: count for res, count in resolutions.items() if count == max(resolutions.values())}
+            max_digits = max([int(''.join([d for d in res if d.isdigit()])) for res in choose_from])
+            targ_res = [res for res in resolutions if str(max_digits) in res][0]
+        else:
+            targ_res = max(resolutions, key=lambda x: resolutions.get(x))
+        return targ_res
+    
+    target_resolution = choose_resolution(resolutions)
+    print(f'Target resolution: {target_resolution}')
+
+
+
+    pattern = r"(?<=assets/).+?(?:/audio_\d+\.mp4|/\d+/\d+/(?:\d+p)\.mp4)"
+    downloaded_chunks = set()
+    
+    for url in urls:
+        if 'audio' not in url and target_resolution not in url:
+            continue
+            
+        identifier = None
+        match = re.search(pattern, url)
+        if match:
+            identifier = match.group(0)
+
+        if identifier in downloaded_chunks:
+            continue
+
+        downloaded_chunks.add(identifier)
+        
+        file_type = ('video', 'audio')['audio' in url]
+        i = (video_counter, audio_counter)[file_type == 'audio']
+        print(f'\nReceiving content from {url}')
+        response = requests.get(url)
+        print('Creating file...')
+        file = f'{file_type}_{i}.mp4'
+        with open(file, 'wb') as f:
+            f.write(response.content)
+        print(f'File {file} created')
+        files.append(file)
+        if file_type == 'audio':
+            audio_counter += 1
+        else:
+            video_counter += 1
+
+    print('\nDownloading accomplished.')
+    return files
+
+
+def merge_files(input_files, output_file='output.mp4', ffmpeg_log='ffmpeg_log.txt'):
+    audio_files = sorted([file for file in input_files if 'audio' in file])
+    print(f'\nTemporary audio files downloaded: {", ".join(audio_files)}')
+    
+    video_files = sorted([file for file in input_files if 'video' in file])
+    print(f'Temporary video files downloaded: {", ".join(video_files)}')
+
+    print('Putting the whole thing together...')
+    
+    #subprocess.run("ffmpeg -i video.mp4 -i audio.mp4 -c copy output.mp4", shell=True)
+
+    if not len(audio_files) and len(video_files):
+        print('\nNo valid content collected!')
+    else:
+        join_files(audio_files, 'audio.mp4')
+        join_files(video_files, 'video.mp4')
+   
+        if os.path.exists('audio.mp4') and os.path.exists('video.mp4'):
+            if os.path.exists(output_file):
+                os.remove(output_file)
+            with open(ffmpeg_log, 'wt', encoding='UTF-8') as log:
+                with open (FFMPEG_CMD, 'rt', encoding='UTF-8') as f:
+                    ffmpeg_cmd = f.read()
+                subprocess.call(ffmpeg_cmd, shell=True, stderr=log)
+            if os.path.exists(output_file):
+                print(f'\n{output_file} has been created! Good job, Gansta!')
+        else:
+            print('\nCan not merge files!')
+
+        for file in (input_files + ['audio.mp4', 'video.mp4']):
+            if os.path.exists(file):
+                os.remove(file)
+        print('\nTemporary files removed.')
