733ae4916def6f713fe00677a174d48fea95422d
ZeRg0912 <serg.021@yandex.ru>
2024-02-06 16:35:24 +0300

    add sup russian lang to x86

--- Keithley/test/test.cpp
+++ Keithley/test/test.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+//#include <locale>
 #include "Keithley.hpp"
 
 int main() {
+    SetConsoleCP(CP_UTF8);
+    SetConsoleOutputCP(CP_UTF8);
     setlocale(LC_ALL, "RUSSIAN");
     system("chcp 1251");
     system("cls");
