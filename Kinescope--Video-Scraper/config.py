1fe8011e9a556c5e8746d3d3e1695f7ecd3e9f9a
Сергей Полевой <serg.021@yandex.ru>
2024-02-13 21:03:41 +0300

    fix

--- Kinescope--Video-Scraper/config.py
+++ Kinescope--Video-Scraper/config.py
@@ -1,8 +1,8 @@
-URL = 'https://netology.ru/profile/program/cppm-14/lessons/314171/lesson_items/1701089'
+URL = 'https://netology.ru/profile/program/cppl-14/lessons/349421/lesson_items/1891575'
 EMAIL = 'serg.021@yandex.ru'
 PASS_PATH = 'data/password.srt'
 OUTPUT = 'output_mix_video.mp4'
 LINKS_LOG = 'data/logs/links_log.txt'
 FFMPEG_LOG = 'data/logs/ffmpeg_log.txt'
-STEP = 150
-DURATION = 180
+STEP = 200
+DURATION = 250
