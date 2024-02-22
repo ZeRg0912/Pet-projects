9a3094016d187f8289c4d3b68dba6cbacccced60
ZeRg0912 <108353609+ZeRg0912@users.noreply.github.com>
2024-02-22 14:12:33 +0300

    Delete Liba directory

--- Liba/Liba.cpp
+++
@@ -1,6 +0,0 @@
-#include "Liba.h"
-
-//bool CinCheck(auto in, std::string type) {
-//	if (typeid(in).name == type.c_str()) return true;
-//	else return false;
-//}
--- Liba/Liba.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <iostream>
-
-bool CinCheck(auto in, std::string type) {
-	if (&typeid(in).name == type.c_str()) return true;
-	else return false;
-}
